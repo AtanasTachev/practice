7bfcd2074eae3999c86962d9b1684298ef4e04b2
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-01-06 10:48:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is a practice app!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BE HAPPY
         </a>
       </header>
     </div>
