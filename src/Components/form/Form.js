9f61c1f301ad3acbeaf73c6c23ae61452655c9d4
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-01-17 13:39:53 +0200

    add info and complete login form

--- src/Components/form/Form.js
+++ src/Components/form/Form.js
@@ -4,12 +4,30 @@ const Form = () => {
     return (
         <div>
             <form class="form">
-                <label for="firstName">First Name</label>
-                <input type="text" id="firstName"/>
-                <label for="male">Male</label>
-                <input type="radio" name="gender" id="male" value="male"></input>
-                <label for="female">Female</label>
-                <input type="radio" name="gender" id="female" value="female"></input>
+                <ul>
+                    <li>
+                        <label for="firstName"><b>First Name</b></label>
+                        <input type="text" id="firstName" placeholder='Ivan'/>
+                    </li>
+                    <li>
+                        <label for="lastName"><b>Last Name</b></label>
+                        <input type="text" id="lastName" placeholder='Ivanov'/>
+                    </li>
+                    <li>
+                        <label for="email"><b>Email</b></label>
+                        <input type="email" id="email" placeholder='ivan@abv.bg'/>
+                    </li>
+                    <li>
+                        <label for="gender"><b>Gender  </b></label>
+                        <label for="male">Male</label>
+                        <input type="radio" name="gender" id="male" value="male"></input>
+                        <label for="female">Female</label>
+                        <input type="radio" name="gender" id="female" value="female"></input>
+                    </li>
+                    <li>
+                        <button type='submit'>Submit</button>
+                    </li>
+                </ul>
 
 
             </form>
