3204c7031615d4fd976cb9f2612047d9fdb14e94
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-01-27 23:39:28 +0200

    add some styles to about

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -5,6 +5,13 @@ const About = () => {
         <div className='about'>
             <h2>About us</h2>
             <p>We are a fast growing community ot lecturers, students and highly motivated professionals who are eager to teach and learn computer science in depth.</p>
+            <p>
+                 We are a human-centric UX & Web Design agency that is passionate, dedicated, and committed toward delivering excellent results.
+            </p>
+            <p>
+                We take pride in our outstanding communication skills, and make it a point to exceed our clients' expectations. The best way to attract and retain customers is by creating memorable online experiences. 
+                Businesses move online  faster than ever, which is why we take time to keep up with the latest in trends and technologies.
+            </p>
         </div>
     )
 }
