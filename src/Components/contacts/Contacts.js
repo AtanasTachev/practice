330eb7368749929dba4f619a024d65ca97556ab3
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-01-29 14:53:47 +0200

    add physical address

--- src/Components/contacts/Contacts.js
+++ src/Components/contacts/Contacts.js
@@ -6,7 +6,8 @@ const Contacts = () => {
             <ul>
                 <li><i class="fab fa-facebook-square"> practice-to-master</i></li>
                 <li><i class="fal fa-mobile"></i> www.practice-to-master.com</li>
-                <i class="fas fa-phone-square"> +359 898 @#$ @#%</i>
+                <li><i class="fas fa-phone-square"> +359 898 @#$ @#%</i></li>
+                <li><i class="fas fa-home"> 15 Lincoln Avn 1458 Detroit Michigan</i></li>
             </ul>
 
         </section>
