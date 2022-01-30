b6140035abea599d3cd785680dcd379b28f93601
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-01-31 00:18:11 +0200

    add css to mentor component

--- src/Components/mentors/Mentors.js
+++ src/Components/mentors/Mentors.js
@@ -3,9 +3,9 @@ import './mentors.css'
 const Mentors = () => {
     return (<div className='box'>
         <ul>
-            <li className='containerBox'>First Name:</li>   
-            <li className='containerBox'>Last Name:</li>
-            <li className='containerBox'>Specialty:</li>            
+            <li className='containerBox'>First Name:<span>Ivan</span></li>   
+            <li className='containerBox'>Last Name:<span>Ivanov</span></li>
+            <li className='containerBox'>Specialty:<span>React JS</span></li>            
         </ul>
     </div>)
 }
