06554b85eda1fe6007d4901bc06dcae703db2dc8
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-02-06 09:42:11 +0200

    add images and css

--- src/Components/gallery/Gallery.js
+++ src/Components/gallery/Gallery.js
@@ -2,10 +2,12 @@ import './gallery.css';
 
 const Gallery = () => {
     return (
-        <ul>
+        <ul className='img'>
             <li>
                 <img src="https://937216.smushcdn.com/2561330/wp-content/uploads/2021/11/Free-Online-Courses-with-Certificates-1024x620.jpg?lossy=1&strip=1&webp=1"></img>
+                <img src="https://www.istockphoto.com/vector/online-education-e-learning-concept-sketch-doodle-horizontal-isolated-copy-space-gm1071675056-286787019?utm_source=pixabay&utm_medium=affiliate&utm_campaign=ADP_illustration_sponsored&utm_content=http%3A%2F%2Fpixabay.com%2Fillustrations%2Ftraining-development-business-396524%2F&utm_term=training+development"></img>
             </li>
+
         </ul>
 
     )
