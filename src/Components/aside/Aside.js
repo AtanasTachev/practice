7af8b8d96bf4178018a7a1e5a20214f1b80be8a4
Atanas Tachev <81210808+AtanasTachev@users.noreply.github.com>
2022-02-19 08:03:40 +0200

    add some functions to aside menu

--- src/Components/aside/Aside.js
+++ src/Components/aside/Aside.js
@@ -1,16 +1,29 @@
+import { useContext, useReducer } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 import './aside.css';
 
 const Aside = () => {
+const { user } = useContext(AuthContext)
+
     return (
     <aside>
         <ul class="aside__ul">
             <li class="btn__aside"><Link to="/"><i class="fal fa-presentation">Practice Courses</i></Link></li>
             <li class="btn__aside"><Link to="/about"><i class="fas fa-history">About</i></Link></li>
             <li class="btn__aside"><Link to="/bestPractices"><i class="fas fa-heart">Favourite Lectures</i></Link></li>
-            <li class="btn__aside"><Link to="/gallery"><i class="fas fa-images">Gallery</i></Link></li>
-            <li class="btn__aside"><Link to="/chooseMentor"><i class="fas fa-user-graduate">Choose Mentor</i></Link></li>
             <li class="btn__aside"><Link to="/contacts"><i class="far fa-id-card">Contacts</i></Link></li>
+            <li class="btn__aside"><Link to="/gallery"><i class="fas fa-images">Gallery</i></Link></li>
+            { user.email ?
+            <>
+            <li className="btn__aside">
+                <Link  to="/cretePractice"><i class="far fa-id-card">Create Practice</i></Link>
+            </li>
+            <li class="btn__aside"><Link to="/chooseMentor"><i class="fas fa-user-graduate">Choose Mentor</i></Link></li>
+            </>
+            :<>
+            </>
+            }
         </ul>
     </aside>
     );
